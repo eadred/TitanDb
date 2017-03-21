7256d091d390e25d6f45c76512ea48815bfd7c3b
Eadred <eadred.birchenough@zuhlke.com>
2017-03-21 09:17:49 +0000

    Basic dev sandbox provisioning

---
+++ Vagrantfile
@@ -0,0 +1,23 @@
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.network "forwarded_port", guest: 22, host: 2222, id: "ssh", disabled: true
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
+
+  config.vm.define :titan do |titan|
+    titan.vm.hostname="titan"
+    titan.vm.network :private_network, ip: "192.168.58.100"
+    titan.vm.network "forwarded_port", guest: 22, host: 5022
+    titan.vm.network "forwarded_port", guest: 9080, host: 9080
+    titan.vm.network "forwarded_port", guest: 9081, host: 9081
+
+    titan.vm.provider :virtualbox do |vb|
+	    vb.name = "titan"
+      vb.memory = 8192
+      vb.cpus = 2
+    end
+
+    titan.vm.provision "shell", path: "provision.sh"
+  end
+
+end
---
+++ provision.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPTDIR=$(dirname $0)
+
+echo "Running provisioning script"
+
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /home/vagrant/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /home/vagrant/.bashrc
+
+# sudo wget http://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz -O /usr/local/spark-2.1.0-bin-hadoop2.7.tgz
+# sudo tar xfvz /usr/local/spark-2.1.0-bin-hadoop2.7.tgz -C /usr/local
+# sudo mv /usr/local/spark-2.1.0-bin-hadoop2.7 /usr/local/spark
+# sudo rm /usr/local/spark-2.1.0-bin-hadoop2.7.tgz
+# sudo chown vagrant:vagrant -R /usr/local/spark
+# export PATH=/usr/local/spark/bin:/usr/local/spark/sbin:$PATH
+# echo 'export PATH=/usr/local/spark/bin:/usr/local/spark/sbin:$PATH' >> /home/vagrant/.bashrc
+#
+# pushd /usr/local/spark/conf
+# echo SPARK_MASTER_WEBUI_PORT=9080 | sudo tee spark-env.sh > /dev/null
+# echo SPARK_WORKER_WEBUI_PORT=9081 | sudo tee -a spark-env.sh > /dev/null
+# sudo chmod a+x spark-env.sh
+# popd
+#
+# start-master.sh -h localhost
+# start-slave.sh spark://localhost:7077 -h localhost
+
+echo "Done"
