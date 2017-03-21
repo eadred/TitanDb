a0fc0a51e8abb5c647f327e1cef9844318019ed1
Eadred <eadred.birchenough@zuhlke.com>
2017-03-21 11:45:24 +0000

    Installing Titan

--- provision.sh
+++ provision.sh
@@ -6,11 +6,27 @@ echo "Running provisioning script"
 
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
+sudo apt-get install unzip
 sudo apt-get install -y openjdk-8-jdk
+
+# Set up Java
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /home/vagrant/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /home/vagrant/.bashrc
+
+# Set up Titan
+pushd /usr/local
+sudo wget http://s3.thinkaurelius.com/downloads/titan/titan-1.0.0-hadoop2.zip -O titan-1.0.0-hadoop2.zip
+sudo unzip titan-1.0.0-hadoop2.zip
+sudo mv titan-1.0.0-hadoop2 titan
+sudo rm titan-1.0.0-hadoop2.zip
+sudo mkdir titan/db
+sudo chmod a+w titan/db
+sudo chmod a+w titan/ext
+export PATH=/usr/local/titan/bin:$PATH
+echo 'export PATH=/usr/local/titan/bin:$PATH' >> /home/vagrant/.bashrc
+popd
 
 # sudo wget http://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz -O /usr/local/spark-2.1.0-bin-hadoop2.7.tgz
 # sudo tar xfvz /usr/local/spark-2.1.0-bin-hadoop2.7.tgz -C /usr/local
