ac628b65e82a6f6740acebdbe23042251e8c8f95
Eadred <eadred.birchenough@zuhlke.com>
2017-03-22 16:56:58 +0000

    Attempting to use remote graph so we can use gremlin-scala on the client side

--- client-app/build.sbt
+++ client-app/build.sbt
@@ -4,6 +4,6 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "com.michaelpollmeier" % "gremlin-scala" % "2.4.2"
+libraryDependencies += "com.michaelpollmeier" % "gremlin-scala_2.12" % "3.2.4.1"
 libraryDependencies += "org.apache.tinkerpop" % "tinkergraph-gremlin" % "3.2.4"
 libraryDependencies += "org.apache.tinkerpop" % "gremlin-driver" % "3.2.4"
--- client-app/src/main/scala/Main.scala
+++ client-app/src/main/scala/Main.scala
@@ -1,9 +1,11 @@
-//import com.tinkerpop.gremlin.scala._
-//import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory
+import gremlin.scala._
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.{TinkerFactory, TinkerGraph}
 
 import scala.collection.JavaConverters._
 import org.apache.tinkerpop.gremlin.driver.{Client, Cluster}
 import org.apache.commons.configuration.PropertiesConfiguration
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection
+import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph
 
 
 /**
@@ -14,15 +16,28 @@ object Main {
     val config = new PropertiesConfiguration("gremlinclient.properties")
 
     val cluster = Cluster.open(config)
-    val client = cluster.connect[Client]()
 
-    val result = client.submit("g.V().values('name')");
+    //Set up a remote source
+    val traversalSource = EmptyGraph.instance().traversal().withRemote(DriverRemoteConnection.using(cluster, "g"))
+
+    //Alternatively set up the traversal source without explicitly creating a cluster using a properties file,
+    // which in turn references a yaml file containing the same config as the config the cluster uses
+    //val remoteConfig = new PropertiesConfiguration("gremlinremote.properties")
+    //val traversalSource = EmptyGraph.instance().traversal().withRemote(remoteConfig)
+
+    val traversal = gremlin.scala.wrap(traversalSource.V()).value[String]("name")
+
+    //The problem is this doesn't work with TitanDB in the backend since this is still using
+    //Tinkerpop 3.0.x which doesn't support remote graphs yet
+    //See https://groups.google.com/forum/#!topic/gremlin-users/N-56-tQRUhQ
+
+    val result = traversal.toList()
 
     System.out.println("Processing results...")
-    result.stream().forEach(r => System.out.println(s"Result was '${r.getString}'"))
+    result.foreach(r => System.out.println(s"Result was '$r'"))
     System.out.println("Done!")
 
-    client.close()
+    traversalSource.close()
     cluster.close()
   }
 }
